7a700cc291a215f0645b53d1adb92fab96dfa107
craigcliff <craigcliff@gmail.com>
2018-01-16 17:47:14 +0200

    added sass variables and built basic grid

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,55 @@
+const { resolve } = require('path')
+
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'gridproj',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'custom float Grid layout' }
+
+
+    modules: [
+        // provide path to the file with resources
+
+        ['nuxt-sass-resources-loader', [
+            resolve(__dirname, 'assets/css/abstracts/_variables.scss'),
+            resolve(__dirname, 'assets/css/abstracts/_mixins.scss'),
+            resolve(__dirname, 'assets/css/base/_base.scss'),
+            resolve(__dirname, 'assets/css/layout/_grid.scss')
+        ]]
+
+
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
+
     /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+     ** Headers of the page
+     */
+    head: {
+        title: 'gridproj',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'custom float Grid layout' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+    /*
+     ** Customize the progress bar color
+     */
+    loading: { color: '#3B8070' },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** Run ESLint on save
+         */
+        extend(config, { isDev, isClient }) {
+            if (isDev && isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,32 @@
 <template>
   <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        gridproj
-      </h1>
-      <h2 class="subtitle">
-        custom float Grid layout
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
+    <div class="row">
+      <div class = "red col-1-of-2">RED </div>
+      <div class =" blue col-1-of-2">BLUE </div>
+    </div>
+
+    <div class="row">
+      <div class = "red col-1-of-3">RED </div>
+      <div class =" blue col-1-of-3">BLUE </div>
+      <div class =" red col-1-of-3">RED </div>
+    
+    </div>
+
+    <div class="row">
+      <div class = "red col-1-of-4">RED </div>
+      <div class =" blue col-1-of-4">BLUE </div>
+      <div class =" red col-1-of-4">RED </div>
+      <div class =" red col-1-of-4">BLUE </div>
+    </div>
+
+    <div class="row">
+      <div class = "red col-1-of-3">RED </div>
+      <div class =" blue col-2-of-3">BLUE </div>
+    </div>
+
+    <div class="row">
+      <div class = "red col-3-of-4">RED </div>
+      <div class =" blue col-1-of-4">BLUE </div>
     </div>
   </section>
 </template>
@@ -26,33 +41,19 @@ export default {
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped lang = "scss">
+
+
+.red{
+  background-color: $color-primary;
+ 
+
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.blue{
+background-color: $color-secondary-light;
+
+
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
